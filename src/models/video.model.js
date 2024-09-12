ca2f13971e84915903c5eb5117fe4ecde89e2a05
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-09-12 17:37:34 +0530

    feat: Add Mongoose model for video data with pagination

---
+++ src/models/video.model.js
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    videofile: {
+      type: String,
+      require: true,
+    },
+    thumbnail: {
+      type: String,
+      require: true,
+    },
+    titel: {
+      type: String,
+      require: true,
+    },
+    description: {
+      type: String,
+      require: true,
+    },
+    duration: {
+      type: Number,
+      require: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    ispublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: truess }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video = mongoose.model("Video", videoSchema);
