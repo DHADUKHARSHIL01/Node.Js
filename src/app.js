6c17235f133cd2e67ccadcb943d64027d2214902
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-09-13 17:57:28 +0530

    fix: Corrected CORS typo and added user routes setup

--- src/app.js
+++ src/app.js
@@ -3,9 +3,10 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
+
 app.use(
   cors({
-    origin: process.env.CROS_ORIGIN,
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
@@ -14,5 +15,10 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+// Routes setup
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
 
 export { app };
