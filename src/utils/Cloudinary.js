4d4ee8ef16b2aa2d04383240ce33feb639a4c419
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-09-14 16:25:15 +0530

    refactor: Improve Cloudinary upload and rename function

--- src/utils/Cloudinary.js
+++ src/utils/Cloudinary.js
@@ -5,20 +5,20 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-  secure: true,
 });
 
-const uploadResult = async (params) => {
+const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
 
-    console.log("file is uplode on cloudinary", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
-    fs.unlink(localFilePath);
+    fs.unlinkSync(localFilePath);
     return null;
   }
 };
+export { uploadOnCloudinary };
