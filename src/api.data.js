241ec5f61e3ae6d67c844b46da5e359af68e3909
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-09-16 15:35:50 +0530

    feat: Add JS file to export user data array

---
+++ src/api.data.js
@@ -0,0 +1,13 @@
+const data = [
+  { name: "Harshil Dhaduk", email: "harshiltest@gmail.com" },
+  { name: "Sam", email: "samtest@email.com" },
+  { name: "Jon", email: "testjon@gmail.com" },
+  { name: "Tester", email: "tester@gmail.com" },
+  { name: "Deno", email: "demo@email.com" },
+  { name: "Harshil Dhaduk", email: "harshiltest@gmail.com" },
+  { name: "Sam", email: "samtest@email.com" },
+  { name: "Jon", email: "testjon@gmail.com" },
+  { name: "Tester", email: "tester@gmail.com" },
+  { name: "Deno", email: "demo@email.com" },
+];
+export default data;
