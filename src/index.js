7f3dd6f7eba0daa9d370af7ec8e06289d1ce6470
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-09-16 15:36:59 +0530

    refactor: Replace Express with basic HTTP server, update errors

--- src/index.js
+++ src/index.js
@@ -1,19 +1,36 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import { app } from "./app.js";
-import { error } from "console";
+// import { app } from "./app.js";
+import http from "http";
 
 dotenv.config({
   path: "./.env",
 });
 
-connectDB()
-  .then((err) => {
-    app.listen(process.env.PORT || 6001, () => {
-      console.log(`Server is running at: ${process.env.PORT}`);
-      console.log("Hello");
-    });
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "content-Type": "'text/html" });
+    res.write("hello");
+    res.end();
   })
-  .catch((err) => {
-    console.error("MongoDB connection failed", err);
+  .listen(process.env.PORT);
+
+connectDB()
+  .then(() => {
+    console.log(`Server is running at: ${process.env.PORT}`);
+    console.log("Hello");
+  })
+
+  .catch((error) => {
+    console.error("MongoDB connection failed", error);
   });
+// connectDB()
+//   .then((err) => {
+//     app.listen(process.env.PORT || 6001, () => {
+//       console.log(`Server is running at: ${process.env.PORT}`);
+//       console.log("Hello");
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("MongoDB connection failed", err);
+//   });
